df52e5e544fdadfd14023d9b8b41db725ddb971f
dbracamontes <danielbracamontes87@gmail.com>
2022-03-14 10:56:44 -0600

    Add files via upload
    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Container } from '@material-ui/core';
+import './App.css';
+import Checkout from './components/order/Checkout';
+import MiniDrawer from './components/header/header';
+
+function App() {
+  return (
+    <div className="App">
+      <MiniDrawer />
+    </div>
+  );
+}
+
+export default App;
